5d74c7c84d70da72f1c0767521e28e48c856e8db
rzbobo <rzbobo@naver.com>
2025-03-06 13:06:20 +0900

    보완할 점 / 유효성검사 및 예외처리 /

--- src/calc02/App.java
+++ src/calc02/App.java
@@ -7,7 +7,6 @@ public class App {
 
         System.out.println("----------------- 명령어 List -----------------");
         System.out.println("exit : 종료");
-        System.out.println("view : 저장된 모든 연산 결과 조회");
         System.out.println("remove0 : 가장 처음 저장된 연산 결과 삭제");
         System.out.println("remove1 : 직전 연산 결과 삭제");
         System.out.println("clear : 연산 결과 모두 삭제");
@@ -22,11 +21,11 @@ public class App {
         while (!keyword.equals("exit")) {
             // 매개 변수 입력
             System.out.println("----------------- 계산기 프로그램 -----------------");
-            System.out.print("첫 번째 숫자를 입력하세요: ");
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
             calculator.setNum1(sc.nextInt());
-            System.out.print("사칙연산 기호를 입력하세요: ");
+            System.out.print("사칙연산 기호를 입력하세요 : ");
             calculator.setOperator(sc.next().charAt(0));
-            System.out.print("두 번째 숫자를 입력하세요: ");
+            System.out.print("두 번째 숫자를 입력하세요 : ");
             calculator.setNum2(sc.nextInt());
             sc.nextLine(); // 버퍼 비우기
 
@@ -34,8 +33,9 @@ public class App {
             // 세터로 매개 변수를 입력했기 때문에 연산에 직접적으로 입력하지 않는다.
             calculator.calculate();
             // 연산 결과 출력
-            System.out.println("연산 결과 : " + calculator.getResult());
+            System.out.println("저장된 연산 결과 : " + calculator.getResult());
 
+            // 키워드 재입력을 위한 반복문
             while (!keyword.equals("another")) {
             System.out.print("다음으로 실행할 명령어를 입력해주세요 : ");
 
@@ -44,17 +44,21 @@ public class App {
             switch (keyword) {
                 case "remove0" -> {
                     calculator.removeFirstResult();
-                    System.out.println("연산 결과 : " + calculator.getResult());
+                    System.out.println("remove0 : 가장 처음 저장된 연산 결과 삭제");
+                    System.out.println("저장된 연산 결과 : " + calculator.getResult());
                 }
                 case "remove1" -> {
                     calculator.removeResult();
-                    System.out.println("연산 결과 : " + calculator.getResult());
+                    System.out.println("remove1 : 직전 연산 결과 삭제");
+                    System.out.println("저장된 연산 결과 : " + calculator.getResult());
                 }
                 case "clear" -> {
                     calculator.clearResult();
-                    System.out.println("연산 결과가 모두 삭제됐습니다.");
+                    System.out.println("clear : 연산 결과 모두 삭제");
+                    System.out.println("저장된 연산 결과가 모두 삭제됐습니다.");
                 }
                 case "exit" -> {
+                    System.out.println("exit : 종료");
                     System.out.println("종료합니다.");
                     return;
                 }
--- src/calc02/Calculator.java
+++ src/calc02/Calculator.java
@@ -11,39 +11,13 @@ public class Calculator {
     private char operator; // 기호
     private ArrayList<String> result = new ArrayList<>();
 
-    // ArrayList<Integer>가 아닌 <Number>여야하는 이유
-    // 결국 나눗셈 결과 값이 실수가 나올 수 있는 계산기이기 때문에
-    // integer로 저장하면 실수값이 나왔을 때 처리하기 곤란
-    // 따라서, 자료형 자체를 number로 선언하는 게 적합하다 생각함.
-    // double로 선언 시, 정수끼리의 연산에서도 소수점이 나올 수 있기에 number가 적합함
-    // 구현을 하다 보니, 연산 결과를 정말로 연산 결과값만 저장하게 되는 것이 아쉽다고 생각
-    // String 방식을 통해 문자열로 받아오기 방식 채택
-
-
-    // operator가 String이 아닌 char인 이유
-    // 어차피 연산자는 단어 하나기 때문에 비교적 간단한 char를 사용하는 것이 적합하다 생각함
-    // 이 과정에서 char 사용 시, 문자 표현할 때 ""이 아닌 ''로 표현하는 것을 다시 배움
-
-    // 3/5일 저녁회의를 통해 유효성 검사를 진행해야 함을 깨달았음.
-    // 0을 포함한 양의 정수임을 num1과 num2에 대해서 유효성 검사를 진행해야 한다.
-    // 당연하게도 operator 값 또한 연산 가능한 연산자에 대해서 유효성 검사를 진행한다.
-    // 나눗셈에 있는 유효성 검사를 한 메서드로 합칠 수 있는지에 대한 여부또한 고민
-    // 3-1 강의에 있는 예외(Exception)과 예외처리(try-catch) 활용
-    // operator
-
-
     // 메서드
 
     // 연산하기
     // setter로 입력값을 가져오기 때문에 매개변수를 메서드에서 직접 받지 않는다.
     public void calculate() {
 
-        // 연산 결과를 두 개로 구분을 해야함
-        // 1) 연산 값 -> opResult
-        // 나눗셈의 실수 값을 고려해 Number로 선언.
-        // 2) 식을 포함한 연산 값 -> result
-        // 어차피 연산 값은 연산 메서드에서만 필요하니 연산 메서드 자체에서만 선언,
-        // 이후 문자열에 합쳐서 2) result 데이터로 저장
+
         Number opResult;
 
         // switch문 작성 시, ':' 작성 방식과 '->' 작성 방식이 존재하는데,
@@ -69,6 +43,7 @@ public class Calculator {
         }
         String history = num1 + " " + operator + " " + num2 + " = " + opResult;
         result.add(history);
+        System.out.println("연산 결과 : " +history);
     }
 
 
